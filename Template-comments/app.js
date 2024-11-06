6e40198ab128a17222e0457593d43517c2fa5d03
BrunoOller <brunobrunelli4@gmail.com>
2024-11-06 12:14:33 -0300

    Quase finalizando o js

--- Template-comments/app.js
+++ Template-comments/app.js
@@ -46,8 +46,150 @@ const data = {
                     createdAt: "1 weeks ago",
                     score: 4,
                     replyingTo: "maxblagun",
-                }
+                    user: {
+                        image: {
+                            png: "./images/avatars/image-ramsesmiron.png",
+                            webp: "./images/avatars/image-ramsesmiron.webp",
+                        },
+                        username: "ramsesmiron",
+                    },
+                },
+                {
+                    parent: 2,
+                    id: 1,
+                    content:
+                        "I couldn't agree more with this.",
+                    createdAt: "2 days ago",
+                    score: 2,
+                    replyingTo: "ramsesmiron",
+                    user: {
+                        image: {
+                            png: "./images/avatars/image-juliusomo.png",
+                            webp: "./images/avatars/image-juliusomo.webp",
+                        },
+                        username: "juliusomo",
+                    },
+                },
             ],
         },
-    ]
+    ],
+};
+
+function appendFrag(frag, parent) {
+    let children = [].slice.call(frag.childNodes, 0);
+    parent.appedChild(frag);
+    return children[0]
+}
+
+const addComment = (body, parentId, replyTo = undefined) => {
+    let commentParent = parentId === 0 ? data.comments : data.comments.filter((c) => c.id == parentId)[0].replies;
+    let newComment = {
+        parent: parentId,
+        id: commentParent.length == 0 ? 1 : commentParent[commentParent.length - 1].id + 1,
+        content: body,
+        createdAt: "Now",
+        replyingTo: replyTo,
+        score: 0,
+        replies: parent == 0 ? [] : undefined,
+        user: data.currentUser,
+    }
+    commentParent.push(newComment);
+    initComments()
+}
+
+const deleteComment = (commentObject) => {
+    if (commentObject.parent == 0) {
+        data.comments = data.comments.filter((e) => e != commentObject);
+    } else {
+        data.comments.filter((e) => e.id === commentObject.parent)[0].replies = data.comments.filter((e) => e.id == commentObject.parent)[0].replies.filter((e) => e != commentObject);
+    }
+    initComments()
+}
+
+const promptDel = (commentObject) => {
+    const modalWrp = document.querySelector(".modal-wrp");
+    modalWrp.classList.remove("invisible");
+    modalWrp.querySelector(".yes").addEventListener("click", () => {
+        deleteComment(commentObject);
+        modalWrp.classList.add("invisible");
+    })
+    modalWrp.querySelector(".no").addEventListener("click", () => {
+        modalWrp.classList.add("invisible");
+    })
+}
+
+const spawnReplyInput = (parent, parentId, replyTo = undefined) => {
+    if (parent.querySelectorAll(".reply-input")) {
+        parent.querySelectorAll(".reply-input").forEach((e) => {
+            e.remove()
+        })
+    }
+}
+
+const inputTemplate = document.querySelector(".reply-input-template");
+const inputNode = inputTemplate.content.cloneNode(true);
+const addedInput = appendFrag(inputNode, parent);
+addedInput.querySelector(".bu-primary").addEventListener("click", () => {
+    let commentBody = addedInput.querySelector(".cmnt-input").value;
+    if (commentBody.length == 0) return;
+    addComment(commentBody, parentId, replyTo)
+})
+
+const createCommentNode = (commentObject) => {
+    const commentTemplate = document.querySelector(".comment-template");
+    let commentNode = commentTemplate.content.cloneNode(true);
+    commentNode.querySelector(".usr-name").textContent = commentObject.user.username;
+    commentNode.querySelector(".usr-img").src = commentObject.user.image.webp;
+    commentNode.querySelector(".score-number").textContent = commentObject.score;
+    commentNode.querySelector(".cmnt-at").textContent = commentObject.createdAt;
+    commentNode.querySelector(".c-body").textContent = commentObject.content;
+    if (commentObject.replyTo) {
+        commentNode.querySelector(".reply-to").textContent = "@" + commentObject.replyingTo;
+        commentNode.querySelector(".score-plus").addEventListener("click", () => {
+            commentObject.score++;
+            initComments();
+        })
+        commentNode.querySelector(".score-minus").addEventListener("click", () => {
+            commentObject.score--;
+            if (commentObject.score < 0) commentObject.score = 0;
+            initComments()
+        })
+        if (commentObject.user.username == data.currentUser.username) {
+            commentNode.querySelector(".comment").classList.add("this-user");
+            commentNode.querySelector(".delete").addEventListener("click", () => {
+                promptDel(commentObject)
+            })
+            commentNode.querySelector(".edit").addEventListener("click", (e) => {
+                const path = e.path[3].querySelector("c-body");
+                if (path.getAttribute("contenteditable") == false || path.getAttribute("contenteditable") == null) {
+                    path.setAttribute("contenteditable", true);
+                    path.focus()
+                } else {
+                    path.removeAttribute("contenteditable")
+                }
+            })
+            return commentNode;
+        }
+
+    } return commentNode
+}
+const appendComment = (parentNode, commentNode, parentId) => {
+    const bu_reply = commentNode.querySelector(".reply");
+    const appendedCmnt = appendFrag(commentNode, parentNode);
+    const replyTo = appendedCmnt.querySelector(".usr-name").textContent;
+    bu_reply.addEventListener("click", () => {
+        if (parentNode.classList.containes("replies")) {
+            spawnReplyInput(parentNode, parentId, replyTo)
+        } else {
+            spawnReplyInput(appendedCmnt.querySelector(".replies"), parentId, replyTo)
+        }
+    })
+}
+
+function initComments(commentList = data.comments, parent = document.querySelector(".comments-wrp")) {
+    parent.innerHTML = "";
+    commentList.forEach((element) => {
+        let parentId = element.parent == 0 ? element.id : element.parent;
+        const comment_node = createCommentNode(element);
+    })
 }
